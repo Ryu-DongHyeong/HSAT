3f09650b6bcbb4578d8aa5c8724b1d575df0739e
doryu <1sm11rjt@naver.com>
2023-12-05 20:58:39 +0900

    초기 설정

---
+++ BaekJoon/BaekJoon/src.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+#include <queue>
+#include <algorithm>
+#include <vector>
+using namespace std;
+
+int main() {
+	cin.tie(0);
+	cout.tie(0);
+	ios::sync_with_stdio(false);
+
+
+}
