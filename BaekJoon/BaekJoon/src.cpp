7fb23b3f9c56a50826f29d47144e3cda6d40482e
doryu <1sm11rjt@naver.com>
2023-12-07 18:53:29 +0900

    s5 2563 색종이 구현

--- BaekJoon/BaekJoon/src.cpp
+++ BaekJoon/BaekJoon/src.cpp
@@ -3,36 +3,35 @@
 #include <algorithm>
 #include <vector>
 using namespace std;
-int arr[101];
+bool arr[101][101];
 
 int main() {
 	cin.tie(0);
 	cout.tie(0);
 	ios::sync_with_stdio(false);
 
-	int N, M;
-	cin >> N >> M;
-	// N 바구니 개수
-	// M 공 넣는 횟수
-
-	for (int i = 1; i <= N; i++) {
-		arr[i] = i;
-	}
-
-	int x, y;
-
-	for (int i = 1; i <= M; i++) {
+	int n;
+	int x = 0;
+	int y = 0;
+	cin >> n;
+	for (int i = 1; i <= n; i++) {
 		cin >> x >> y;
-		int sum_temp = x + y;
-		int cha_temp = (y - x + 1) / 2;
-		for (int j = 0; j < cha_temp; j++) {
-			int temp = arr[x + j];
-			arr[x + j] = arr[sum_temp - x - j];
-			arr[sum_temp - x - j] = temp;
+		for (int j = 1; j <= 10; j++) {
+			for (int k = 1; k <= 10; k++) {
+				arr[x + j][y + k] = 1;
+			}
 		}
 	}
 
-	for (int i = 1; i <= N; i++) {
-		cout << arr[i]<<" ";
+	int ans = 0;
+
+	for (int i = 1; i <= 100; i++) {
+		for (int j = 1; j <= 100; j++) {
+			if (arr[i][j] == 1) {
+				ans++;
+			}
+		}
 	}
+
+	cout << ans;
 }
